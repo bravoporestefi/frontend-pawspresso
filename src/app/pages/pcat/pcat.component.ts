f89b3687359c3167eb828eb9974e55c4622723ae
bravoporestefi <bravoporestefi@gmail.com>
2024-06-18 06:14:39 +0200

    estilos varios

--- src/app/pages/pcat/pcat.component.ts
+++ src/app/pages/pcat/pcat.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pcat',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pcat.component.html',
   styleUrl: './pcat.component.scss'
 })
--- src/app/pages/ppeople/ppeople.component.ts
+++ src/app/pages/ppeople/ppeople.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ppeople',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ppeople.component.html',
   styleUrl: './ppeople.component.scss'
 })
