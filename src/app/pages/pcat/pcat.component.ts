fa090b6acb4e3c2d7ac2302eeb031353f0e5008a
bravoporestefi <bravoporestefi@gmail.com>
2024-06-18 08:42:29 +0200

    procat

--- src/app/pages/pcat/pcat.component.ts
+++ src/app/pages/pcat/pcat.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductsService } from '../../servicios/products.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pcat',
   standalone: true,
-  imports: [RouterLink],
+  providers: [ProductsService],
+  imports: [RouterLink, HttpClientModule, CommonModule],
   templateUrl: './pcat.component.html',
   styleUrl: './pcat.component.scss'
 })
 export class PcatComponent {
-
+  products: any = [];
+  constructor(private productsService: ProductsService) {
+    this.productsService.getProducts().subscribe((products) => {
+      this.products = products;
+    })
+  }
 }
