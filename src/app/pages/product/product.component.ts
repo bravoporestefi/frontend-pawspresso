3b9945ca5b6d9aa304022520dcaabae858214183
bravoporestefi <bravoporestefi@gmail.com>
2024-06-18 09:07:15 +0200

    botones de redirigir en product

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
